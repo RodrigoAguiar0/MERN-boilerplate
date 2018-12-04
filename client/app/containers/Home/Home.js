dd1507ad98fe4104887a3cf123ff4ae008858366
Rodrigo Aguiar <rodrigoaguiar0@gmail.com>
2018-12-04 20:07:49 -0300

    termino do front-end

---
+++ client/app/containers/Home/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import 'whatwg-fetch';
+
+/*newCounter() {
+  fetch('/api/counters', { method: 'POST' })
+    .then(res => res.json())
+    .then(json => {
+      let data = this.state.counters;
+      data.push(json);
+
+      this.setState({
+        counters: data
+      });
+    });
+}*/
+
+const Home = () => (
+  <div>
+    <p><a href="/profile">View Profile</a></p>
+  </div>
+)
+
+export default Home;
---
+++ client/app/containers/Profile/Profile.js
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import StockInfo from '../../components/Profile/StockInfo';
+
+class Profile extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isLoading: true, 
+            error: null,
+            results: [],
+        };
+
+
+        this.fetchProfileStockInfo = this.fetchProfileStockInfo.bind(this);
+    }
+
+    componentWillMount(){
+        this.fetchProfileStockInfo();
+    }
+
+    fetchProfileStockInfo(){
+        console.log('fetchProfileStockInfo');
+        fetch('/api/stock/portfolio', { method: 'POST' })
+            .then(res => res.json())
+            .then(json => {
+                if(json.success){
+                    this.setState({
+                        isLoading: false,
+                        results: json.results
+                    });
+                } else{
+                    this.setState({
+                        isLoading: false,
+                        error: json.message,
+                    });
+                }
+            });
+    }
+
+    render () {
+        const{
+            error,
+            isLoading,
+            results,
+        } = this.state;
+
+        if(isLoading){
+            return(
+                <div>
+                    <p>Loading...</p>
+                </div>
+            );
+        }
+
+        if(error){
+            return(
+                <div style={{backgroundColor: '#610B21'}}>
+                    <p style={{color: '#FFF'}}>
+                    {error}
+                    </p>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <p>Profile</p>
+                {
+                    results.map(result => <StockInfo data={result}/>)
+                }
+            </div>
+        );
+    }
+}
+
+export default Profile;
